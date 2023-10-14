5eb9cbfe49e05f862c3dc7c6de40446cfc4e7ad2
khaphan-github <phanhoangkha01@gmail.com>
2023-10-14 10:54:06 +0000

    Connected to IO Serverr

---
+++ src/app/components/NotFoundRoom/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './NotFoundRoom.module.css';
+
+
+const NotFoundRoom = () => {
+  return (
+    <div>NotFoundRoom</div>
+  );
+};
+
+export default NotFoundRoom;
