0035a024c91d04c18cff40a86cb7058429b4840a
hovinh414 <hovinh414@gmail.com>
2023-10-23 22:20:00 +0700

    fix tam layout chat

--- src/app/components/Chat/ChatDetail.jsx
+++ src/app/components/Chat/ChatDetail.jsx
@@ -8,97 +8,152 @@ import User4 from "../../../../public/images/hero4.jpg";
 import { IOChanel, SocketIOService } from "@/scripts/socket";
 import { useSearchParams } from "next/navigation";
 import { ReceiverComponent } from "../ChessBoard/ChessBoard";
-import styles from "../../../../public/css/chess/styles.module.css"
+import styles from "../../../../public/css/chess/styles.module.css";
 import ScrollToBottom from "react-scroll-to-bottom";
 import Message from "@mui/icons-material/Message";
 
-const ChatDetail = ({ socket, username}) => {
-    const [showModal, setShowModal] = useState(false);
-    const [messageList, setMessageList] = useState([]);
-    const [newMessage, setNewMessage] = useState("");
-    const [currentMessage, setCurrentMessage] = useState("");
+const ChatDetail = ({ socket, username }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [messageList, setMessageList] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [currentMessage, setCurrentMessage] = useState("");
 
-    const searchParams = useSearchParams();
-    const room = searchParams.get("room") ?? "";
-    const sendMessage = async () => {
-        if (currentMessage !== ""){
-          const messageData = {
-            room: room,
-            author: username,
-            message: currentMessage,
-            time:
-              new Date(Date.now()).getHours() +
-              ":" +
-              new Date(Date.now()).getMinutes(),
-          };
-        //   setCurrentMessage(currentMessage);
-        socket.emit(IOChanel.CHAT_CHANEL_SEND, messageData);
-          setCurrentMessage('');
-        }
+  const searchParams = useSearchParams();
+  const room = searchParams.get("room") ?? "";
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      //   setCurrentMessage(currentMessage);
+      await socket.emit(IOChanel.CHAT_CHANEL_SEND, messageData);
+      setCurrentMessage("");
     }
-    useEffect(()=>{
-        socket.on(IOChanel.CHAT_CHANEL_RECEIVE, (data) => {
-            setMessageList((list) => [...list, data]);
-            console.log('Data receive from chat detail: '+ JSON.stringify(messageList));
-          });
-    }, [socket])
-    return (
-        <div
-            className='fixed p-10 bg-indigo-800'
-            id='main'
-            style={{
-                width: '330px',
-                height: '600px',
-                top: '30px',
-                right: '10px',
-                borderRadius: '25px',
-            }}
-        >
-            <div>
-                {messageList.map((messageContent, key) => {
-                    return (
-                    <div
-                    key = {key}
-                    className={
-                        messageContent.author == username
-                            ? styles.containerReviceMess
-                            : styles.containerSendMess
-                        }
-                    >
-                        <div className={styles.mess}>
-                                <div className={styles.detail}>{messageContent.message}</div>
-                                <Image className={styles.avatar} src={messageContent.author == username ?User1 :User4} alt="User Image" />
-                                <div className={styles.name}>{messageContent.author}</div>
-                        </div>
-                    </div>
-                    );
-                })}
-                <div className="absolute bottom-0 left-0 w-full p-4 border-t border-gray-300">
-                    <div className="flex items-center">
-                        <input
-                            className="w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500 text-black"
-                            type="text"
-                            value={currentMessage}
-                            placeholder="Hey..."
-                            onChange={(event) => {
-                                setCurrentMessage(event.target.value);
-                            }}
-                            onKeyPress={(event) => {
-                                event.key === "Enter" && sendMessage();
-                            }}
-                        />
-                        <SendIcon
-                            onClick={() => {
-                                sendMessage();
-                            }}
-                            className="ml-2 h-6 w-6 text-gray-100"
-                        />
-                    </div>
-                </div>
+  };
+  useEffect(() => {
+    socket.on(IOChanel.CHAT_CHANEL_RECEIVE, (data) => {
+      setMessageList((list) => [...list, data]);
+      console.log(
+        "Data receive from chat detail: " + JSON.stringify(messageList)
+      );
+    });
+  }, [socket]);
+  return (
+    <div
+      className="fixed p-10 bg-indigo-800"
+      id="main"
+      style={{
+        width: "330px",
+        height: "600px",
+        top: "30px",
+        right: "10px",
+        borderRadius: "25px",
+      }}
+    >
+      <div
+        style={{
+          overflowY: "auto" /* Thêm thanh cuộn dọc */,
+          maxHeight: "490px" /* Chiều cao tối đa cho phần tin nhắn */,
+          scrollbarWidth: "thin" /* Hiển thị thanh cuộn dọc trên Firefox */,
+        }}
+      >
+        <div style={{
+            marginTop:"45px",
+        }}>
+          <div className={styles.containerSendMess}>
+            {/* phần con */}
+            <div className={styles.mess}>
+              <div className={styles.detail}>Xin chào hehehehe!</div>
+              <Image className={styles.avatar} src={User1} alt="User Image" />
+              <div className={styles.name}>Phan Hoàng Kha</div>
+              <div className={styles.time}>22:00</div>
             </div>
-            {showModal && <ModalMain setShowModal={setShowModal} />}
+          </div>
+          {/* phần cha */}
+          <div className={styles.containerReviceMess}>
+            {/* phần con */}
+            <div className={styles.mess}>
+              <div className={styles.detail}>Chào Kha!</div>
+              <Image className={styles.avatar} src={User4} alt="User Image" />
+              <div className={styles.name}>Hồ Thành Vinh</div>
+              <div className={styles.time}>21:50</div>
+            </div>
+          </div>
+          <div className={styles.containerSendMess}>
+            <div className={styles.mess}>
+              <div className={styles.detail}>Kéo api xong chưa?</div>
+              <Image className={styles.avatar} src={User1} alt="User Image" />
+              <div className={styles.name}>Phan Hoàng Kha</div>
+              <div className={styles.time}>22:00</div>
+            </div>
+          </div>
+          <div className={styles.containerReviceMess}>
+            {/* phần con */}
+            <div className={styles.mess}>
+              <div className={styles.detail}>Chào Kha!</div>
+              <Image className={styles.avatar} src={User4} alt="User Image" />
+              <div className={styles.name}>Hồ Thành Vinh</div>
+              <div className={styles.time}>22:05</div>
+            </div>
+          </div>
+          <div className={styles.containerSendMess}>
+            <div className={styles.mess}>
+              <div className={styles.detail}>Kéo api xong chưa?</div>
+              <Image className={styles.avatar} src={User1} alt="User Image" />
+              <div className={styles.name}>Phan Hoàng Kha</div>
+              <div className={styles.time}>22:10</div>
+            </div>
+          </div>
         </div>
-    );
+        {messageList.map((messageContent, key) => {
+          return (
+            <div
+              key={key}
+              className={
+                messageContent.author == username
+                  ? styles.containerReviceMess
+                  : styles.containerSendMess
+              }
+            >
+              <div className={styles.mess}>
+                <div className={styles.detail}>{messageContent.message}</div>
+                <Image className={styles.avatar} src={User4} alt="User Image" />
+                <div className={styles.name}>{messageContent.author}</div>
+              </div>
+            </div>
+          );
+        })}
+        <div className="absolute bottom-0 left-0 w-full p-4 border-t border-gray-300">
+          <div className="flex items-center">
+            <input
+              className="w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500 text-black"
+              type="text"
+              value={currentMessage}
+              placeholder="Hey..."
+              onChange={(event) => {
+                setCurrentMessage(event.target.value);
+              }}
+              onKeyPress={(event) => {
+                event.key === "Enter" && sendMessage();
+              }}
+            />
+            <SendIcon
+              onClick={() => {
+                sendMessage();
+              }}
+              className="ml-2 h-6 w-6 text-gray-100"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatDetail;
