864e4d29b4939befbe441ea19b1d8a6616fb32d2
Lê Đỗ Thành Đạt <ledothanhdat1208@gmail.com>
2023-10-25 09:38:19 +0700

    Update config.ts

--- src/scripts/config.ts
+++ src/scripts/config.ts
@@ -1,5 +1,5 @@
-const API_URL = 'china-chess-nodejs-be.railway.internal:3100/api/v1';
-const SOCKET_URL = 'china-chess-nodejs-be.railway.internal:3200';
+const API_URL = 'https://china-chess-nodejs-be.railway.internal:3100/api/v1';
+const SOCKET_URL = 'https://china-chess-nodejs-be.railway.internal:3200';
 const URL = 'https://chinese-chess-production.up.railway.app/'
 
 export { API_URL, SOCKET_URL, URL }
